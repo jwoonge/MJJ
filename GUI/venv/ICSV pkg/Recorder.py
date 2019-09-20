e3fcf745728d7d98f1a6a0fc31f00cef828fe069
신주환 <jason-shin94@hanmail.net>
2019-09-20 21:39:23 +0900

    GUIversion1.1

--- GUI/venv/ICSV pkg/Recorder.py
+++ GUI/venv/ICSV pkg/Recorder.py
@@ -1,28 +1,85 @@
-from scipy.io.wavfile import write
-import sounddevice as sd
+import pyaudio
+import SoundHandler
 
+def testRECORDER() :
+  testRECORDER.count += 1
+  formatname='.wav'
+  count_s = str(testRECORDER.count)
+  filename = 'test' + count_s + formatname
+  CHUNK = 1
+  FORMAT = pyaudio.paInt16
+  CHANNELS = 1
+  RATE = 16000
+  RECORD_SECONDS = 3
 
-def trainRECORDER():
-    trainRECORDER.count += 1
-    count_s = str(trainRECORDER.count)
-    formatname = '.wav'
-    filename = 'train' + count_s + formatname
-    fs = 16000  # Sample rate
-    seconds = 3  # Duration of recording
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
-    sd.wait()  # Wait until recording is finished
-    write(filename, fs, myrecording)  # Save as WAV file
+  p = pyaudio.PyAudio()
 
-def testRECORDER():
-    testRECORDER.count += 1
-    count_s = str(testRECORDER.count)
-    formatname = '.wav'
-    filename = 'test' + count_s + formatname
-    fs = 16000  # Sample rate
-    seconds = 5  # Duration of recording
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
-    sd.wait()  # Wait until recording is finished
-    write(filename, fs, myrecording)  # Save as WAV file
+  stream = p.open(format=FORMAT,
+                  channels=CHANNELS,
+                  rate=RATE,
+                  input=True,
+                  frames_per_buffer=CHUNK)
+
+  print("Start to record the audio.")
+
+  frames = []
+
+  for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+  print("Recording is finished.")
+  stream.stop_stream()
+  stream.close()
+  p.terminate()
+
+  value = []
+  for i in range(0, len(frames)):
+    ret = int.from_bytes(frames[i], 'little', signed=True)
+    value.append(ret)
+
+  output_rec = SoundHandler.Sound(value, 1)
+  output_rec.WriteWav_self(filename)
+
+def trainRECORDER() :
+  trainRECORDER.count += 1
+  formatname='.wav'
+  count_s = str(testRECORDER.count)
+  filename = 'train' + count_s + formatname
+  CHUNK = 1
+  FORMAT = pyaudio.paInt16
+  CHANNELS = 1
+  RATE = 16000
+  RECORD_SECONDS = 3
+
+  p = pyaudio.PyAudio()
+
+  stream = p.open(format=FORMAT,
+                  channels=CHANNELS,
+                  rate=RATE,
+                  input=True,
+                  frames_per_buffer=CHUNK)
+
+  print("Start to record the audio.")
+
+  frames = []
+
+  for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+  print("Recording is finished.")
+  stream.stop_stream()
+  stream.close()
+  p.terminate()
+
+  value = []
+  for i in range(0, len(frames)):
+    ret = int.from_bytes(frames[i], 'little', signed=True)
+    value.append(ret)
+
+  output_rec = SoundHandler.Sound(value, 1)
+  output_rec.WriteWav_self(filename)
 
 
 trainRECORDER.count = 0
--- GUI/venv/ICSV pkg/txtReader.py
+++ GUI/venv/ICSV pkg/txtReader.py
@@ -2,7 +2,7 @@ import pandas as pd
 from random import *
 
 def Readcsv() :
-  src = pd.read_csv('C:\\Users\\JooHwan\\PycharmProjects\\GUI\\venv\CSV source\QuizBank.csv', engine= 'python')
+  src = pd.read_csv('CSV source\QuizBank.csv', engine= 'python')
   i = randint(0, 2)
   Readstring = src.columns[i]
   return Readstring
